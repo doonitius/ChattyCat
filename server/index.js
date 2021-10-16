a03cc90bbefda8019f6f6afdd53b5c2c2e85b162
doonitius <dkitsakulpaisan@gmail.com>
2021-10-16 19:48:12 +0700

    add user to send message in room

--- server/index.js
+++ server/index.js
@@ -4,6 +4,13 @@ const socketio = require('socket.io');
 const http = require('http');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
+const {
+    userJoin,
+    getCurrentUser,
+    userLeave,
+    getRoomUsers
+} = require('./middleware/users.js');
 
 require("dotenv").config();
 
@@ -12,26 +19,39 @@ app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-const port = process.env.PORT || 3000;
-
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/view/view.html')
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/view/view.html')
+// })
+
+app.use(express.static(path.join(__dirname, 'view')));
 
 app.use('/api/', require('./test'))
 
 io.on('connection', (socket) => {
-    //console.log('user connected');
-    socket.emit('message', 'Hi user');
-    socket.broadcast.emit('message', 'User joined');
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+    socket.on('joinRoom', ({ username, room }) => {
+        const user = userJoin(socket.id, username, room);
+        
+        socket.join(user.room);
+        socket.emit('message', 'Hi user '+ user.username);
+        socket.broadcast.to(user.room).emit('message', user.username + ' has joined');
+
     });
+    
+    //console.log('user connected');
+    socket.on('chat message', (msg) => {
+        const user = getCurrentUser(socket.id);
+
+        io.to(user.room).emit('chat message', msg);
+    });
+
     socket.on('disconnect', () => {
-        io.emit('message', 'User disconnected');
+
+        const user = userLeave(socket.id);
+
+        io.to(user.room).emit('message', user.username + ' disconnected');
     });
 });
 
@@ -41,7 +61,7 @@ mongoose.connect(process.env.dbConnection,
     console.log('connected to DB')
     );
 
+const port = process.env.PORT || 3000;
+
 server.listen(port, () => {
     console.log('Server is running on port ' + port)});
-
-module.exports = app;
