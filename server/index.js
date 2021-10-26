5b0cf766cd47ba36fa30aea64e874950a6929d99
nightsyber25 <preem.2010@hotmail.com>
2021-10-26 23:47:26 +0700

    index

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ require("dotenv").config();
 
 app.use(cors());
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const server = http.createServer(app);
@@ -43,27 +43,45 @@ app.get('/', (req, res) => {
 
 // app.use('/api/', require('./test'))
 
+var clients = {};
+
 // check first if ChatID exist, if not, create. 
 // if exist backend send old chat, frontend send parameter 
 // call this from localhost:3000/?username=username&room=room (await this when creating chat room)
 io.on('connection', (socket) => {
-    console.log("connected");
-    console.log(socket.connected);
+    console.log("connected to socket");
+    console.log(socket.id, "has joined");
     console.log(socket.handshake.query.username);
     console.log(socket.handshake.query.room);
 
     var username = socket.handshake.query.username;
     var room = socket.handshake.query.room;
 
+
+    socket.on("signin", (id, targetId) => {
+        console.log("ID:" + id);
+        clients[id] = socket;
+        console.log(clients);
+        const user = userJoin(socket.id, id, targetId);
+        socket.join(user.room);
+        console.log(user);
+    });
+
+    socket.on("message", (msg) => {
+        console.log(msg);
+        let targetId = msg.targetId;
+        if (clients[targetId]) clients[targetId].emit("message", msg);
+    });
+
     socket.on('joinRoom', () => {
         const user = userJoin(socket.id, username, room);
         console.log(user);
         socket.join(user.room);
-        socket.emit('message', 'Hi user '+ user.username);
+        socket.emit('message', 'Hi user ' + user.username);
         socket.broadcast.to(user.room).emit('message', user.username + ' has joined');
 
     });
-    
+
     //console.log('user connected');
     socket.on('chat message', (msg) => {
         const user = getCurrentUser(socket.id);
@@ -77,18 +95,19 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
 
         const user = userLeave(socket.id);
-
+        console.log("disconnected");
         io.to(user.room).emit('message', user.username + ' disconnected');
     });
 });
 
 
 
-mongoose.connect(process.env.dbConnection, 
-    { useNewUrlParser: true, 
-        useUnifiedTopology: true }, () =>
+mongoose.connect(process.env.dbConnection, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }, () =>
     console.log('connected to DB')
-    );
+);
 
 const port = process.env.PORT || 3000;
 
