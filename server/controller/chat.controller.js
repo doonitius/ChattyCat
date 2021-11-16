b3e37e499e28a73ee45c428a5f9d84d526bfc124
doonitius <dkitsakulpaisan@gmail.com>
2021-11-16 22:26:50 +0700

    send empid all chat

--- server/controller/chat.controller.js
+++ server/controller/chat.controller.js
@@ -1,31 +1,57 @@
 const message = require('../model/message')
 const userChat = require('../model/userChat')
 const chatInfo = require('../model/chatInfo')
+const userNamePass = require('../model/userNamePass')
 
 ///////////////////////////////////////////
 exports.allChat = async (req, res) => {
     var nomessage = [{text : " ", time: new Date}];
+    var noEmpID = "-";
     var userChatVerify = await userChat.findOne({employeeID: req.body.employeeID}, { "_id": 0, "__v": 0, "employeeID": 0 });
-    var checkGroup = await userChat.findOne({employeeID: req.body.employeeID})
+    //var checkGroup = await userChat.findOne({employeeID: req.body.employeeID})
     if(!userChatVerify){ res.status(400).send({messsage: "no chat"});}
-    var chatID = [];
+    //var chatID = [];
     var empID = [];
-    for (var i = 0; i < checkGroup.chatVerify.length; i++) {
-        if (checkGroup.chatVerify[i].isGroup == false) {
-            chatID.push(checkGroup.chatVerify[i].chatID);
-        }
-    }
-    for (var i = 0; i < chatID.length; i++) {
-        const emID = await chatInfo.findOne({_id: chatID[i]}, {member: { $slice: 1}})
-        empID.push(emID.member[0].employeeID);
-    }
-    console.log(empID);
+    // for (var i = 0; i < checkGroup.chatVerify.length; i++) {
+    //     if (checkGroup.chatVerify[i].isGroup == false) {
+    //         chatID.push(checkGroup.chatVerify[i].chatID);
+    //     }
+    // }
+    // for (var i = 0; i < chatID.length; i++) {
+    //     const emID = await chatInfo.findOne({_id: chatID[i]}, {member: { $slice: 1}})
+    //     empID.push(emID.member[0].employeeID);
+    // }
+    // console.log(empID);
     for (var i = 0; i < userChatVerify.chatVerify.length; i++) {
+        
         const chatinfo = await chatInfo.findOne({_id: userChatVerify.chatVerify[i].chatID});
-        if(chatinfo === undefined) {console.log("sdfsdf");continue;}
+        if(chatinfo === undefined) {continue;}
+
+        
+        if(!userChatVerify.chatVerify[i].isGroup){
+            const findEmpID = await userNamePass.findOne({userName: userChatVerify.chatVerify[i].chatName}, {"password": 0, "_id": 0});
+            if(await findEmpID === undefined) {
+                var foundEmpID = noEmpID;
+                console.log("not found" + foundEmpID);
+            }
+            else{
+                var foundEmpID = findEmpID.employeeID;
+            }
+        }
+        else{
+            var foundEmpID = noEmpID;
+        }
+            Object.keys(userChatVerify.chatVerify[i]).map(
+            function(object){
+                userChatVerify.chatVerify[i][object]["employeeID"] = foundEmpID;
+            }
+        );
+
+
         if (await chatinfo.previewChat.length === 0){
             Object.assign(chatinfo.previewChat, nomessage);
         } 
+
         Object.keys(userChatVerify.chatVerify[i]).map(
             function(object){
                 userChatVerify.chatVerify[i][object]["previewChat"] = chatinfo.previewChat
