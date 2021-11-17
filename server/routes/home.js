a4bd6e2ffed628ecf03c3dc17a11ecdaebc60909
nightsyber25 <preem.2010@hotmail.com>
2021-11-17 20:00:40 +0700

    change get to post in home/chat

--- server/routes/home.js
+++ server/routes/home.js
@@ -1,11 +1,13 @@
 const controller = require('../controller/home.controller')
-const {verifyToken,
-    getRefreshTokens, 
+const {
+    verifyToken,
+    getRefreshTokens,
     setRefreshTokens,
-    logout} = require('../middleware/auth')
+    logout
+} = require('../middleware/auth')
 
-module.exports = function (app) {
-    app.get('/home/origin', getRefreshTokens, verifyToken, controller.home,);
-    app.get('/home/chat', getRefreshTokens, verifyToken,  controller.indivChat);
+module.exports = function(app) {
+    app.get('/home/origin', getRefreshTokens, verifyToken, controller.home, );
+    app.post('/home/chat', getRefreshTokens, verifyToken, controller.indivChat);
     app.post('/home/search', getRefreshTokens, verifyToken, controller.search);
 }
