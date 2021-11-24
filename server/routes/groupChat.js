d934aef0f4540e26a1f75bcf78d1e272e5e8a6ed
nightsyber25 <preem.2010@hotmail.com>
2021-11-24 21:50:59 +0700

    get to post in /group

--- server/routes/groupChat.js
+++ server/routes/groupChat.js
@@ -1,11 +1,12 @@
 const controller = require('../controller/group.controller')
-const {verifyToken,
+const {
+    verifyToken,
     getRefreshTokens
-    } = require('../middleware/auth')
+} = require('../middleware/auth')
 
-module.exports = function (app) {
+module.exports = function(app) {
     app.post('/group/create', getRefreshTokens, verifyToken, controller.createGroup);
-    app.post('/group/invite',getRefreshTokens, verifyToken, controller.invite);
+    app.post('/group/invite', getRefreshTokens, verifyToken, controller.invite);
     app.post('/group/invite/search', getRefreshTokens, verifyToken, controller.search);
-    app.get('/group', getRefreshTokens, verifyToken, controller.group)
-}    
+    app.post('/group', getRefreshTokens, verifyToken, controller.group)
+}
